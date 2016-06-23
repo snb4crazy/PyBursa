ab0d7e54b6232d1ebac34123d6f81b66a74ca6e2
Serhii <serge.sergeevich@gmail.com>
2016-06-23 20:07:33 +0300

    HomeWork3:Polls: Detail View & Template

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
+from django.http import Http404
 
 
 def index(request):
@@ -10,7 +11,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
