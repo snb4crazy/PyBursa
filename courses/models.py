a9bb474a8189b2b997396369ffe5381cc4b59d38
Serhii <serge.sergeevich@gmail.com>
2016-06-24 15:40:15 +0300

    HomeWork:Courses: Models

---
+++ courses/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=150)
+    description = models.TextField(max_length=1000)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    subject = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    course = models.ForeignKey(Course)
+    order = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return self.subject
+
