134d648ceb4b9b54fdfbf78d9a04f6eab713f4dd
Serhii <serge.sergeevich@gmail.com>
2016-06-27 16:03:44 +0300

    HomeWork:Students:Admin: Fix Search

--- students/admin.py
+++ students/admin.py
@@ -9,14 +9,14 @@ fullname.short_description = 'full name'
 
 
 class StudentAdmin(admin.ModelAdmin):
-    search_fields = ['surname', 'email']
+    search_fields = ['surname', 'email', 'name']
     list_filter = ['courses']
     list_display = [fullname, 'email', 'skype']
     filter_horizontal = ('courses',)
     fieldsets = [
-       ('Personal info', {'fields': ('name','surname','date_of_birth',)}),
-       ('Contact info', {'fields': ('email','phone','address','skype',)}),
-       (None, {'fields': ('courses',)})
+       ('Personal info', {'fields': ('name', 'surname', 'date_of_birth', )}),
+       ('Contact info', {'fields': ('email', 'phone', 'address', 'skype', )}),
+       (None, {'fields': ('courses', )})
     ]
 
 admin.site.register(Student, StudentAdmin)
