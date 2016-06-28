a5644f9eb5576baefa098ab662538fb6c3acd239
Serhii <serge.sergeevich@gmail.com>
2016-06-28 21:14:11 +0300

    Feedback: Fix Routing

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from feedbacks import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.FeedbackView.as_view(), name='feedback'),
 ]
--- feedbacks/views.py
+++ feedbacks/views.py
@@ -9,13 +9,15 @@ from feedbacks.forms import FeedbackForm
 class FeedbackView(CreateView):
 
     model = Feedback
-    fields = '__all__'
     success_url = reverse_lazy('feedback')
     template_name = "feedback.html"
     form_class = FeedbackForm
 
     def form_valid(self, form):
         app = form.save()
+        '''
+        todo fix: mail raises exception
+        '''
         mail_admins(app.subject, app.message, fail_silently=False)
         messages.success(self.request, "Thank you for your feedback! We will keep in touch with you very soon!")
         return super(FeedbackView, self).form_valid(form)
