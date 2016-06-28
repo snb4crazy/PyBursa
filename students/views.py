9aedfce97bdf6a3d4832091515ec1f24d1486862
Serhii <serge.sergeevich@gmail.com>
2016-06-28 17:50:22 +0300

    Forms:Students: Student List Template

--- students/views.py
+++ students/views.py
@@ -1,28 +1,10 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.core.urlresolvers import reverse_lazy
 from students.models import Student
-
-
-def list_view(request):
-    students_list = []
-    student_info = {}
-
-    if request.GET:
-        course_id = request.GET['course_id']
-        students_set = Student.objects.filter(courses=course_id)
-    else:
-        students_set = Student.objects.all()
-    for student in students_set:
-        student_info['id'] = student.id
-        student_info['full_name'] = student.surname + ' ' + student.name
-        student_info['address'] = student.address
-        student_info['skype'] = student.skype
-        student_info['courses'] = student.courses.all()
-        students_list.append(student_info)
-        student_info = {}
-    return render(request, 'students/list.html', {'students_list': students_list})
 
 
 def detail(request, id):
@@ -76,3 +58,24 @@ class StudentDeleteView(DeleteView):
         messages.success(self.request, "Info on %s has been sucessfully deleted." % app.full_name())
         app = app.delete()
         return super(StudentDeleteView, self).form_valid(form)
+
+
+class StudentListView(ListView):
+    model = Student
+    paginate_by = 2
+
+    def get_queryset(self):
+        course_id = self.request.GET.get('course_id', None)
+        if course_id:
+            students = Student.objects.filter(courses__id=course_id)
+        else:
+            students = Student.objects.all()
+        return students
+
+
+class StudentDetailView(DetailView):
+    model = Student
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentDetailView, self).get_context_data(**kwargs)
+        return context
