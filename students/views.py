88c7fae516c9b698e0aace58f7cfa7fac1830850
Serhii <serge.sergeevich@gmail.com>
2016-06-28 17:18:35 +0300

    Forms:Students: Extend Views

--- students/views.py
+++ students/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from students.models import Student
 
 
@@ -27,3 +29,50 @@ def detail(request, id):
     student = Student.objects.get(id=id)
     student_courses = student.courses.all()
     return render(request, 'students/detail.html', {'student': student, 'courses': student_courses})
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    success_url = reverse_lazy('students:list_view')
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentCreateView, self).get_context_data(**kwargs)
+        context['title'] = "Student registration"
+        return context
+
+    def form_valid(self, form):
+        app = form.save()
+        messages.success(self.request, "Student %s has been successfully added." % app.full_name())
+        return super(StudentCreateView, self).form_valid(form)
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    success_url = reverse_lazy('students:list_view')
+    template_name_suffix = '_update_form'
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentUpdateView, self).get_context_data(**kwargs)
+        context['title'] = "Student info update"
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request,"Info on the student has been sucessfully changed.")
+        return super(StudentUpdateView, self).form_valid(form)
+
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    success_url = reverse_lazy('students:list_view')
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentDeleteView, self).get_context_data(**kwargs)
+        context['title'] = "Student info suppression"
+        return context
+
+    def form_valid(self, form):
+        app = Student.objects.get(self.pk)
+        messages.success(self.request, "Info on %s has been sucessfully deleted." % app.full_name())
+        app = app.delete()
+        return super(StudentDeleteView, self).form_valid(form)
