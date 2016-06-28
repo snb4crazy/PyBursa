b0a8c58aeeafa8251b433529610813ed592254ad
Serhii <serge.sergeevich@gmail.com>
2016-06-28 19:06:01 +0300

    Forms:Courses:Templates: Fix CRUD Views Logic

--- courses/views.py
+++ courses/views.py
@@ -6,12 +6,6 @@ from django.core.urlresolvers import reverse_lazy, reverse
 from courses.models import Course, Lesson
 
 
-def detail(request, id):
-    course = Course.objects.get(id=id)
-    lessons = Lesson.objects.filter(course_id=id)
-    return render(request, 'courses/detail.html', {'course': course, 'lessons': lessons, 'id': id})
-
-
 def index(request):
     courses = Course.objects.only('name', 'short_description', 'id')
     return render(request, 'index.html', {"courses": courses})
@@ -24,7 +18,7 @@ class CourseDetailView(DetailView):
     context_object_name = "course"
 
     def get_context_data(self, **kwargs):
-        context = super(CourseDetailView,self).get_context_data(**kwargs)
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
         context["title"] = "Course detail"
         pk = self.kwargs['pk']
         context["lessons"] = Lesson.objects.filter(course_id = pk)
@@ -33,8 +27,9 @@ class CourseDetailView(DetailView):
 
 class CourseCreateView(CreateView):
     model = Course
+    fields = '__all__'
     success_url = reverse_lazy('index')
-    template_name = "courses/add.html"
+    template_name = "courses/course_form.html"
 
     def get_context_data(self, **kwargs):
         context = super(CourseCreateView, self).get_context_data(**kwargs)
@@ -49,8 +44,9 @@ class CourseCreateView(CreateView):
 
 class CourseUpdateView(UpdateView):
     model = Course
+    fields = '__all__'
     success_url = reverse_lazy('index')
-    template_name = "courses/edit.html"
+    template_name = "courses/course_update_form.html"
 
     def get_context_data(self, **kwargs):
         context = super(CourseUpdateView, self).get_context_data(**kwargs)
@@ -58,7 +54,7 @@ class CourseUpdateView(UpdateView):
         context["pk"] = self.kwargs['pk']
         return context
 
-    def form_valid(self,form):
+    def form_valid(self, form):
         form.save()
         messages.success(self.request, u'The changes have been saved.')
         return super(CourseUpdateView, self).form_valid(form)
@@ -71,7 +67,8 @@ class CourseUpdateView(UpdateView):
 
 class CourseDeleteView(DeleteView):
     model = Course
-    template_name = "courses/remove.html"
+    fields = '__all__'
+    template_name = "courses/course_confirm_delete.html"
     success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
@@ -88,7 +85,8 @@ class CourseDeleteView(DeleteView):
 
 class LessonCreateView(CreateView):
     model = Lesson
-    template_name = "courses/add-lesson.html"
+    fields = '__all__'
+    template_name = "courses/lesson_form.html"
 
     def get_context_data(self, **kwargs):
         context = super(LessonCreateView, self).get_context_data(**kwargs)
