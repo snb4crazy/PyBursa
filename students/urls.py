09779f16003800b296d4d0dfd915c059d07485a4
Serhii <serge.sergeevich@gmail.com>
2016-06-28 17:18:57 +0300

    Forms:Students: Routing

--- students/urls.py
+++ students/urls.py
@@ -19,4 +19,7 @@ from students import views
 urlpatterns = [
     url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.StudentCreateView.as_view(), name='add'),
+    url(r'^edit/(?P<pk>\d+)/$', views.StudentUpdateView.as_view(), name='edit'),
+    url(r'^remove/(?P<pk>\d+)/$', views.StudentDeleteView.as_view(), name='remove'),
 ]
