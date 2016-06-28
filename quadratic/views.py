75d30164b20b094f2f830bbf2dbc051947d2df75
Serhii <serge.sergeevich@gmail.com>
2016-06-28 16:55:47 +0300

    Forms:Quadratic: View Logic

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render
+from quadratic.forms import QuadraticForm
 
 
 def quadratic_results(request):
-    data = {}
-    d = x1 = x2 = None
-    is_valid_parameters = True
+    if request.method == 'GET':
+        form = QuadraticForm(request.GET)
+        disc = {}
+        text_result = {}
+        if form.is_valid():
+            a = int(request.GET['a'])
+            b = int(request.GET['b'])
+            c = int(request.GET['c'])
+            disc['message'] = "Дискриминант: "
+            disc['value'] = b**2 - 4*a*c
 
-    for k, v in request.GET.items():
-        try:
-            data[str(k)] = int(v)
-        except ValueError:
-            data[str(k)] = str(v)
-            is_valid_parameters = False
+            if disc['value'] < 0:
+                text_result['message'] = "Дискриминант меньше нуля, уравнение не имеет действительных решений."
+            elif disc['value'] == 0:
+                x = (-b + disc['value'] ** (1/2.0)) / 2*a
+                text_result['message'] = "Дискриминант равен нулю, уравнение имеет один действительный корень: x1=x2 = "
+                text_result['value'] = x
+            else:
+                x1 = (-b + disc['value'] ** (1 / 2.0)) / 2*a
+                x2 = (-b - disc['value'] ** (1 / 2.0)) / 2*a
+                text_result['message'] = "Квадратное уравнение имеет два действительных корня: "
+                text_result['value'] = u"x1 = %.1f, x2 = %.1f" % (x1, x2)
 
-    if is_valid_parameters and not data['a'] == 0:
-        d = data['b'] ** 2 - 4 * data['a'] * data['c']
-        if d == 0:
-            x1 = x2 = -data['b'] / 2.0 * data['a']
-        elif d > 0:
-            x1 = (-data['b'] + d ** (1 / 2.0)) / (2 * data['a'])
-            x2 = (-data['b'] - d ** (1 / 2.0)) / (2 * data['a'])
+        content = {"disc": disc, "text_result": text_result, 'form': form}
 
-    sorted_data = sorted(data.items(), key=lambda x: x[0])
-    return render(request, 'results.html', {'data': sorted_data, 'd': d, 'x1': x1, 'x2': x2})
+        return render(request, 'quadratic/results.html', content)
+
+    else:
+        form = QuadraticForm()
+
+    return render(request, 'quadratic/results.html', {'form': form})
