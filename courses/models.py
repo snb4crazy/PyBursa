c53980174cd3d8ae56530ffe88407da50eabfc6d
Serhii <serge.sergeevich@gmail.com>
2016-06-29 19:00:37 +0300

    Coaches: Course Model Integration

--- courses/models.py
+++ courses/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from coaches.models import Coach
 
 
 class Course(models.Model):
     name = models.CharField(max_length=50)
     short_description = models.CharField(max_length=150)
     description = models.TextField(max_length=1000)
+    coach = models.ForeignKey(Coach, null=True, blank=True, related_name='coach_courses')
+    assistant = models.ForeignKey(Coach, null=True, blank=True, related_name='assistant_courses')
 
     def __unicode__(self):
         return self.name
